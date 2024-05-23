b9a01c2de55b037984ab97c55f476d8a07765f72
Gruszek <101717513+Gruszek@users.noreply.github.com>
2024-05-23 12:15:48 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -8,7 +8,7 @@
 <h2>Wynik Formularza</h2>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $name = htmlspecialchars($_GET['name']);
     $email = htmlspecialchars($_GET['email']);
 
